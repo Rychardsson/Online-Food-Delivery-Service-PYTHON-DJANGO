0b2e6f7738155d07ebb6454967168cc0b2ce037f
Rychardsson <rychardssong2@gmail.com>
2023-12-06 17:10:07 -0300

    Update Restaurant-profile.py

--- Restaurant-profile.py
+++ Restaurant-profile.py
@@ -3,6 +3,17 @@ class Restaurant:
         self.name = name
         self.location = location
         self.menu = Menu()
+        self.pricing = Pricing()
+
+    def update_profile(self, name, location):
+        self.name = name
+        self.location = location
+
+    def display_profile(self):
+        print(f"Restaurant: {self.name}, Location: {self.location}")
+        self.menu.display_menu()
+        self.pricing.display_pricing()
+
 
 class Menu:
     def __init__(self):
@@ -11,6 +22,12 @@ class Menu:
     def add_item(self, item):
         self.items.append(item)
 
+    def display_menu(self):
+        print("Menu:")
+        for item in self.items:
+            print(f"- {item.name}: {item.description} - ${item.price:.2f}")
+
+
 class ItemMenu:
     def __init__(self, name, description, price):
         self.name = name
@@ -18,27 +35,31 @@ class ItemMenu:
         self.price = price
 
 
-class menu:
-  def ___init__(self, ):
+class Pricing:
+    def __init__(self):
+        self.prices = {}
+
+    def set_price(self, item_name, price):
+        self.prices[item_name] = price
+
+    def display_pricing(self):
+        print("Pricing:")
+        for item_name, price in self.prices.items():
+            print(f"- {item_name}: ${price:.2f}")
 
 
-class itemMenu:
-  def __init__(self, valor):
-    self.valor
+my_restaurant = Restaurant("Restaurante", "Localização")
 
-
-my_restaurant = Restaurant("Restaurante ", "Localização")
-
-# Criando itens do menu
 item1 = ItemMenu("Prato1", "Descrição do Prato 1", 15.99)
 item2 = ItemMenu("Prato2", "Descrição do Prato 2", 12.50)
 
-# Adicionando itens ao menu do restaurante
 my_restaurant.menu.add_item(item1)
 my_restaurant.menu.add_item(item2)
 
-#informações do restaurante e do menu
+my_restaurant.display_profile()
+
 print(f"Restaurante: {my_restaurant.name}, {my_restaurant.location}")
 print("Menu:")
 for item in my_restaurant.menu.items:
     print(f"- {item.name}: {item.description} - ${item.price:.2f}")
+
